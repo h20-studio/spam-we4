eee0c439c90d80675c5e33417c583dd0719902e3
H20 Studio <89566553+hendarofficialyt@users.noreply.github.com>
2021-08-26 12:15:56 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+apt upgrade
+apt update
+pkg update
+pkg upgrade
+pkg install wget
+pkg install pip
+pkg install openssh
+pkg install ssh
+pkg install git
+pkg install php
+pkg install python
+pkg install python2
+pkg install python3
+pkg install curl
+pkg install requests
+pkg install nano
+pkg install figlet
+pkg install mc
